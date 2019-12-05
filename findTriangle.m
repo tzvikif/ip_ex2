10639e3712d077e9a039d36f76610efc69985344
tzvikif <tzvikif@gmail.com>
2019-12-05 22:48:28 +0200

    findTriangle with variable arguments

--- findTriangle.m
+++ findTriangle.m
@@ -1,3 +1,19 @@
-function [p1,p2,p3,rowNum] = findTriangle(P,pointsList,mesh)
+function [p1,p2,p3,rowNum] = findTriangle(P,pointsList,mesh,varargin)
+    if(nargin == 3)
+        %% straight forward
+        fprintf('straight forward\n');
+    elseif(nargin == 4)
+        extraParam1 = varargin{1};
+        %% cleverer way
+        fprintf('cleverer way\n');
+        disp(extraParam1);
+    elseif(nargin == 5)
+        extraParam1 = varargin{1};
+        extraParam2 = varargin{2};
+        fprintf('cleverer way\n');
+        disp(extraParam1);
+        disp(extraParam2);
+    end
+    
     
 end
